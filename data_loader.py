2e8255638109e03614864fe2a0866b1c7adf639f
Miaoqi Chu mac <mqichu@anl.gov>
2020-07-01 22:31:51 -0500

    initial commit

---
+++ data_loader.py
@@ -0,0 +1,103 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+from scipy.optimize import curve_fit
+import os
+import h5py
+
+
+hdf_dict = {
+    'Iq': '/exchange/partition-mean-total',
+    'ql_sta': '/xpcs/sqlist',
+    'ql_dyn': '/xpcs/dqlist',
+    't0': '/measurement/instrument/detector/exposure_period',
+    't_el': '/exchange/tau',
+    'g2': '/exchange/norm-0-g2',
+    'g2_err': '/exchange/norm-0-stderr',
+    'Int_2D': '/exchange/pixelSum'
+}
+
+
+def read_file(fields, fn, prefix='./data'):
+    res = []
+    with h5py.File(os.path.join(prefix, fn), 'r') as HDF_Result:
+        for field in fields:
+            val = np.squeeze(HDF_Result.get(hdf_dict[field]))
+            res.append(val)
+
+    return res
+
+
+class DataLoader(object):
+    def __init__(self, prefix, file_list):
+        self.size = len(file_list)
+        self.prefix = prefix
+        self.file_list = file_list
+        # self.label = ['Iq', 'g2', 'Int_2D', 'ql_sta', 'ql_dyn', 't_el']
+        # self.label = ['Iq', 'g2', 'ql_sta', 'ql_dyn', 't_el']
+        # self.data = self.load_data(self.label, file_list)
+
+    def read_data(self, labels, file_list=None, mask=None):
+        if file_list is None:
+            file_list = self.file_list
+
+        if mask is None:
+            mask = np.ones(shape=len(file_list), dtype=np.bool)
+
+        data = []
+        for n, fn in enumerate(file_list):
+            if mask[n]:
+                data.append(read_file(labels, fn, self.prefix))
+
+        np_data = {}
+        for n, label in enumerate(labels):
+            temp = [x[n] for x in data]
+            np_data[label] = np.array(temp)
+
+        return np_data
+
+    def average(self, baseline=1.03, chunk_size=256):
+        labels = ['Iq', 'g2', 'g2_err', 'Int_2D']
+        g2 = self.read_data(['g2'], self.file_list)['g2']
+        mask = np.mean(g2[:, -10:, 1], axis=1) < baseline
+
+        steps = (len(mask) + chunk_size - 1) // chunk_size
+        result = {}
+        for n in range(steps):
+            beg = chunk_size * (n + 0)
+            end = chunk_size * (n + 1)
+            end = min(len(mask), end)
+            slice0 = slice(beg, end)
+            values = self.read_data(labels, file_list=self.file_list[slice0],
+                                    mask=mask[slice0])
+            if n == 0:
+                for label in labels:
+                    result[label] = np.sum(values[label], axis=0)
+            else:
+                for label in labels:
+                    result[label] += np.sum(values[label], axis=0)
+
+        num_points = np.sum(mask)
+        for label in labels:
+            result[label] = result[label] / num_points
+
+        return result
+
+    def plot_g2(self, ax=None):
+        if ax is None:
+            fig, ax = plt.subplots(1, 1)
+
+        print(len(self.data))
+        print(len(self.data[1]))
+        for n in range(self.size):
+            ax.plot(self.data[n][1][0, 1])
+
+        plt.show()
+
+if __name__ == "__main__":
+    flist = os.listdir('./data')
+    dv = DataViewer('./data', flist)
+    dv.average()
+    # dv.plot_g2()
+
---
+++ gui.py
@@ -0,0 +1,78 @@
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5.QtWidgets import QFileDialog, QTableWidgetItem, QFileSystemModel
+from PyQt5.QtCore import QAbstractItemModel
+from PyQt5.QtCore import QObject, pyqtSlot, QDir
+import sys
+import os
+from pyqtgraph import PlotWidget, ImageWindow
+import pyqtgraph as pg
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from data_loader import DataLoader
+
+
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__() # Call the inherited classes __init__ method
+        uic.loadUi('xpcs.ui', self) # Load the .ui file
+        self.show() # Show the GUI
+        self.selected_record = set()
+
+        flist = os.listdir('./data')
+        self.dv = DataLoader('./data', flist)
+        self.avg = self.dv.average()
+
+        import numpy as np
+        x = np.random.normal(size=1000)
+        y = np.random.normal(size=1000)
+        # self.graphWidget.plot(x, y, pen=None, symbol='o')
+        avg = self.dv.read_data(['Int_2D'])['Int_2D']
+        avg = np.log10(avg + 1E-8)
+        xvals = np.arange(avg.shape[0])
+        self.graphWidget.setImage(avg, xvals=xvals)
+
+        # model = QFileSystemModel()
+        # model.setRootPath(QDir.currentPath())
+        # self.treeView.setModel(model)
+        # self.treeView.setRootIndex(model.index(QDir.currentPath()))
+
+    def load_path(self):
+        # f = QFileDialog.getExistingDirectory(self, 'Open directory',
+        #                                      '/User/mqichu',
+        #                                      QFileDialog.ShowDirsOnly)
+        # self.centralWidget.file_panel.work_dir.setValue(f)
+        f = './'
+        self.work_dir.setText(f)
+        all_files = os.listdir(f)
+
+        self.file_list.clear()
+
+        for x in all_files:
+            # filter configure files
+            if x.startswith('.'):
+                continue
+
+            self.file_list.addItem(x)
+            row_position = self.file_table.rowCount()
+            self.file_table.insertRow(row_position)
+            self.file_table.setItem(row_position, 0, QTableWidgetItem(x))
+
+
+    def add_target(self):
+        for x in self.file_list.selectedIndexes():
+            f = x.data()
+            if f not in self.selected_record:
+                self.selected_record.add(f)
+                self.selected_list.addItem(f)
+
+    def remove_target(self):
+        for x in self.selected_list.selectedIndexes():
+            f = x.data()
+            self.selected_record.remove(f)
+            self.selected_list.takeItem(x.row())
+
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = Ui()
+    app.exec_()
