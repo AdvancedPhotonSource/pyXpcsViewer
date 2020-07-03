39b9c49c667d356db373cecd229a46b414d040ca
Miaoqi Chu mac <mqichu@anl.gov>
2020-07-03 08:48:07 -0500

    implement reduced file label

--- data_loader.py
+++ data_loader.py
@@ -28,17 +28,67 @@ def read_file(fields, fn, prefix='./data'):
 
     return res
 
+# the following functions are copied from:
+# https://stackoverflow.com/questions/2892931
+def long_substr(data):
+    substr = ''
+    if len(data) > 1 and len(data[0]) > 0:
+        for i in range(len(data[0])):
+            for j in range(len(data[0])-i+1):
+                if j > len(substr) and is_substr(data[0][i:i+j], data):
+                    substr = data[0][i:i+j]
+    return substr
+
+def is_substr(find, data):
+    if len(data) < 1 and len(find) < 1:
+        return False
+    for i in range(len(data)):
+        if find not in data[i]:
+            return False
+    return True
+
+
+def create_id(in_list, repeat=2, keep_slice=None):
+    """
+    :param in_list: input file name list
+    :param repeat: number of repeats to remove common string
+    :param keep_slice: the slice in the original string to keep, if not given,
+        then use the segment before the first underscore.
+    :return: label list with minimal information redundancy
+    """
+    if len(in_list) < 1:
+        return []
+
+    if keep_slice is None:
+        idx = in_list[0].find('_')
+        keep_slice = slice(0, idx + 1)
+
+    keep_str = in_list[0][keep_slice]
+    if keep_str[-1] != '_':
+        keep_str = keep_str + '_'
+
+    for n in range(repeat):
+        substr = long_substr(in_list)
+        in_list = [x.replace(substr, '') for x in in_list]
+
+    in_list = [keep_str + x for x in in_list]
+    return in_list
+
 
 class DataLoader(object):
     def __init__(self, prefix, file_list):
         self.size = len(file_list)
         self.prefix = prefix
         self.file_list = file_list
+        self.id_list = create_id(self.file_list)
         self.get_g2_data()
 
+    def plot_saxs(self):
+
+
     def get_saxs(self):
-        avg = self.read_data(['Int_2D'])['Int_2D']
-        return avg
+        ans = self.read_data(['Int_2D'])['Int_2D']
+        return ans
 
     def get_g2_data(self):
         labels = ['g2', 'g2_err']
