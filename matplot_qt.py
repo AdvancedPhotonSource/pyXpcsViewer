46dc390b2bf66d883aa2d32a795c94e7143d8215
Miaoqi Chu mac <mqichu@anl.gov>
2020-07-05 15:03:16 -0500

    add auto_scale method

--- matplot_qt.py
+++ matplot_qt.py
@@ -1,5 +1,6 @@
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
+import numpy as np
 
 
 class MplCanvas(FigureCanvasQTAgg):
@@ -9,10 +10,17 @@ class MplCanvas(FigureCanvasQTAgg):
         # self.axes = fig.add_subplot(111)
         super(MplCanvas, self).__init__(self.fig)
         self.axes = None
+        self.obj = None
 
     def subplots(self, n, m, **kwargs):
         self.axes = self.fig.subplots(n, m, **kwargs)
         return self.axes
+
+    def clear(self):
+        self.clear_axes()
+        self.fig.clear()
+        self.axes = None
+        self.obj = None
 
     def clear_axes(self):
         if self.axes is None:
@@ -21,3 +29,43 @@ class MplCanvas(FigureCanvasQTAgg):
             for ax in self.axes.ravel():
                 ax.clear()
 
+    def auto_scale(self):
+        if self.axes is None:
+            return
+        else:
+            for ax in self.axes.ravel():
+                ax.relim()
+                ax.autoscale_view(True, True, True)
+
+    def update_lin(self, loc, x, y):
+        if self.obj is None:
+            return
+        lin_obj, = self.obj['lin'][loc]
+        lin_obj.set_data(x, y)
+        return
+
+    def update_err(self, loc, x, y, y_error):
+        if self.obj is None:
+            return
+        err_obj = self.obj['err'][loc]
+        adjust_yerr(err_obj, x, y, y_error)
+        return
+
+
+# https://github.com/matplotlib/matplotlib/issues/4556
+def adjust_yerr(err_obj, x, y, y_error):
+    # not using error top / bot bar;
+    # ln, (err_top, err_bot), (bars, ) = err_obj
+    ln, _, (bars, ) = err_obj
+    ln.set_data(x, y)
+
+    yerr_top = y + y_error
+    yerr_bot = y - y_error
+
+    # err_top.set_ydata(yerr_top)
+    # err_bot.set_ydata(yerr_bot)
+
+    new_segments = [np.array([[x, yt], [x, yb]]) for
+                    x, yt, yb in zip(x, yerr_top, yerr_bot)]
+
+    bars.set_segments(new_segments)
