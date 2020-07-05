88b634e0282b85d06c4a4f9e25509ec6d740a908
Miaoqi Chu mac <mqichu@anl.gov>
2020-07-05 17:56:16 -0500

    working gui with err_msg window

--- gui.py
+++ gui.py
@@ -14,17 +14,6 @@ import numpy as np
 import time
 # from xpcs_ui import
 
-# from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
-# from matplotlib.figure import Figure
-#
-#
-# class MplCanvas(FigureCanvasQTAgg):
-#
-#     def __init__(self, parent=None, width=5.1, height=3.2, dpi=100):
-#         fig = Figure(figsize=(width, height), dpi=dpi)
-#         self.axes = fig.add_subplot(111)
-#         super(MplCanvas, self).__init__(fig)
-
 
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
@@ -32,26 +21,24 @@ class Ui(QtWidgets.QMainWindow):
         uic.loadUi('xpcs.ui', self)
         self.show()
         self.dl = None
-        self.fl = None
         self.cache = None
         self.load_path()
         self.g2_cache = {}
 
     def load_data(self):
-        if (len(self.fl.target_list)) == 0:
+        if (len(self.dl.target_list)) == 0:
             return
-        self.dl = DataLoader(prefix=self.fl.cwd,
-                             file_list=self.fl.target_list)
 
         self.mf1.subplots(2, 1)
         self.mf1.draw()
         self.prepare_g2()
+        self.btn_load_data.setEnabled(False)
         # self.plot_g2()
         # for n in range(data.shape[0]):
         #     self.mf1.axes[1].plot(res['Int_t_statistics'][n, :, 0], '-o', lw=1, alpha=0.5)
         # self.mf1.axes[1].imshow((res['Int_t'][:, :, 0]).T, aspect='auto')
 
-    def prepare_g2(self, max_q=0.015, max_tel=0.31, num_col=4):
+    def prepare_g2(self, max_q=0.0092, max_tel=0.31, num_col=4):
         res, _, _ = self.dl.get_g2_data()
         ql_dyn_list = res['ql_dyn'].ravel()
         ql_dyn_list = list(set(ql_dyn_list))
@@ -62,9 +49,13 @@ class Ui(QtWidgets.QMainWindow):
         ql_idx = np.argmin(np.abs(max_q - np.array(ql_dyn_list)))
 
         ql_dyn_list = ['%.4f' % (x + 1E-4) for x in ql_dyn_list]
+        prev_ql_idx = self.cb_q_max.currentIndex()
         self.cb_q_max.clear()
         self.cb_q_max.addItems(ql_dyn_list)
-        self.cb_q_max.setCurrentIndex(ql_idx)
+        if prev_ql_idx != -1:
+            self.cb_q_max.setCurrentIndex(prev_ql_idx)
+        else:
+            self.cb_q_max.setCurrentIndex(ql_idx)
 
         t_el_list = res['t_el'].ravel()
         t_el_list = list(set(t_el_list))
@@ -75,9 +66,12 @@ class Ui(QtWidgets.QMainWindow):
 
         t_el_list = ['%.2e' % (x * 1.01) for x in t_el_list]
         # only list 20 points because the list can be very long
+        self.cb_tel_max.setCurrentIndex(t_el_idx - (len(t_el_list) - 20))
+        # t_el_idx = t_el_idx - (len(t_el_list) - 20)
+
+        prev_tel_idx = self.cb_tel_max.currentIndex()
         self.cb_tel_max.clear()
         self.cb_tel_max.addItems(t_el_list[-20:])
-        self.cb_tel_max.setCurrentIndex(t_el_idx - (len(t_el_list) - 20))
 
         self.mf2.clear()
 
@@ -86,22 +80,26 @@ class Ui(QtWidgets.QMainWindow):
     def plot_g2(self, max_points=3):
         if max_points in [False, None]:
             max_points = 3
-
-        num_col = 4
-        # add 1 to convert index to count
-        num_fig = int(self.cb_q_max.currentIndex()) + 1
-        num_points = min(len(self.fl.target_list), max_points)
+        num_points = min(len(self.dl.target_list), max_points)
+        if num_points == 0:
+            return
 
         # read user defined values
         max_q = float(self.cb_q_max.currentText())
         max_tel = float(self.cb_tel_max.currentText())
         offset = self.sb_offset.value()
+        num_col = 4
 
-        # read ql_dyn
+        # add 1 to convert index to count
+        # num_fig = int(self.cb_q_max.currentIndex()) + 1
+        num_fig = np.sum(np.array(self.g2_cache['ql_dyn']) <= max_q)
 
         # adjust canvas size according to number of images
         num_row = (num_fig + num_col - 1) // num_col
-        if self.mf2.axes is None or self.mf2.axes.shape != (num_row, num_col):
+        if self.mf2.axes is None or \
+                self.mf2.axes.shape != (num_row, num_col) or \
+                self.dl.hash(max_points) != self.dl.g2_cache['hash_val']:
+            print('create new fig')
             canvas_size = max(600, 200 * num_row)
             self.mf2.setMinimumSize(QtCore.QSize(0, canvas_size))
             self.mf2.fig.clear()
@@ -110,10 +108,12 @@ class Ui(QtWidgets.QMainWindow):
             self.dl.create_template_g2(self.mf2, self.g2_cache['ql_dyn'],
                                        num_points, num_fig=num_fig)
 
-        self.mf2.draw()
+            self.mf2.draw()
         bounds = self.check_number()
-        self.dl.plot_g2(handler=self.mf2, max_q=max_q, max_tel=max_tel,
-                        offset=offset, bounds=bounds)
+        err_msg = self.dl.plot_g2(handler=self.mf2, max_q=max_q, max_tel=max_tel,
+                                  offset=offset, bounds=bounds)
+        self.err_msg.clear()
+        self.err_msg.insertPlainText('\n'.join(err_msg))
 
 
     def load_path(self):
@@ -123,8 +123,8 @@ class Ui(QtWidgets.QMainWindow):
         # self.centralWidget.file_panel.work_dir.setValue(f)
         f = './data/files2.txt'
         self.work_dir.setText(f)
-        self.fl = FileLocator(f)
-        self.update_box(self.fl.source_list, mode='source')
+        self.dl = DataLoader(f)
+        self.update_box(self.dl.source_list, mode='source')
 
         # for debug
         self.list_view_source.selectAll()
@@ -144,25 +144,40 @@ class Ui(QtWidgets.QMainWindow):
 
     def add_target(self):
         target = []
+        prev_hash = self.dl.hash(-1)
+        print('prev', self.dl.target_list)
         for x in self.list_view_source.selectedIndexes():
             target.append(x.data())
 
-        self.fl.add_target(target)
-        self.update_box(self.fl.target_list, mode='target')
+        self.dl.add_target(target)
+        print('curr', self.dl.target_list)
+        print('-------------------------')
+        self.update_box(self.dl.target_list, mode='target')
+
+        curr_hash = self.dl.hash(-1)
+        if prev_hash != curr_hash:
+            self.btn_load_data.setEnabled(True)
 
     def remove_target(self):
+        prev_hash = self.dl.hash(-1)
         rmv_list = []
         for x in self.list_view_target.selectedIndexes():
             rmv_list.append(x.data())
-        self.fl.remove_target(rmv_list)
-        self.update_box(self.fl.target_list, mode='target')
+
+        self.dl.remove_target(rmv_list)
+        self.update_box(self.dl.target_list, mode='target')
+
+        curr_hash = self.dl.hash(-1)
+        if prev_hash != curr_hash:
+            if len(self.dl.target_list) >= 1:
+                self.btn_load_data.setEnabled(True)
 
     def trie_search(self):
         val = self.filter_str.text()
         if len(val) == 0:
-            self.update_box(self.fl.source_list, mode='source')
+            self.update_box(self.dl.source_list, mode='source')
             return
-        num, self.cache = self.fl.search(val)
+        num, self.cache = self.dl.search(val)
         self.update_box(self.cache, mode='source')
         self.list_view_source.selectAll()
 
