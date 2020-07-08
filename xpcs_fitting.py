c26c3c97ca7b13cf2d5d5628d7cc9d5b66af1406
Miaoqi Chu mac <mqichu@anl.gov>
2020-07-07 22:30:42 -0500

    add fit_tau; change fit_xpcs input

--- xpcs_fitting.py
+++ xpcs_fitting.py
@@ -1,44 +1,50 @@
 import numpy as np
 import scipy as sp
 from scipy.optimize import curve_fit
+from scipy.stats import linregress as sp_stats
 import traceback
-
+from sklearn import linear_model
 
 def single_exp(x, tau, bkg, cts):
     return cts * np.exp( -2 * x / tau) + bkg
 
+def fit_tau(qd, tau, tau_err):
+    x = np.log(qd).reshape(-1, 1)
+    y = np.log(tau).reshape(-1, 1)
+    dy = tau / tau_err
+    reg = linear_model.LinearRegression()
+    reg.fit(x, y, sample_weight=dy)
+    x2 = np.linspace(np.min(x) - 0.1, np.max(x) + 0.1, 128)
+    y2 = reg.predict(x2.reshape(-1, 1))
+    return reg.coef_, reg.intercept_, np.exp(x2).ravel(), np.exp(y2).ravel()
 
-def fit_xpcs(res, idx, tslice, qslice, b):
+
+def fit_xpcs(tel, qd, g2, g2_err, b):
     """
-    :param res: g2_data
-    :param idx:
-    :param max_q:
-    :param contrast:
+    :param tel: t_el
+    :param qd: ql_dyn
+    :param g2: g2 [time, ql_dyn]
+    :param g2_err: [time, ql_dyn]
+    :param b: bounds
     :return:
     """
-    t_el = res['t_el'][idx, tslice]
-    q = res['ql_dyn'][idx, qslice]
-
-    g2 = res['g2'][idx, tslice, qslice]
-    g2_err = res['g2_err'][idx, tslice, qslice]
-
     fit_x = np.logspace(-5, 0.5, num=128)
 
     p0_guess = [np.sqrt(b[0][0] * b[1][0]),
                 0.5 * (b[0][1] + b[1][1]),
                 0.5 * (b[0][2] + b[1][2])]
 
-    fit_val = np.zeros(shape=(q.size, 7))
+    fit_val = np.zeros(shape=(qd.size, 7))
     fit_result = []
-    for n in range(q.size):
+    for n in range(qd.size):
         err = g2_err[:, n]
         result = {'num_zero_err': np.sum(err < 1E-6)}
         avg = np.mean(err[err > 1E-6])
         err[err <= 1E-6] = avg
-        fit_val[n, 0] = q[n]
+        fit_val[n, 0] = qd[n]
 
         try:
-            popt, pcov = curve_fit(single_exp, t_el, g2[:, n],
+            popt, pcov = curve_fit(single_exp, tel, g2[:, n],
                                    p0=p0_guess,
                                    sigma=err,
                                    bounds=b)
@@ -57,3 +63,5 @@ def fit_xpcs(res, idx, tslice, qslice, b):
             fit_result.append(result)
 
     return fit_result, fit_val
+
+
