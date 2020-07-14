8453375ae2f1690b87fe7588efc72f5adc52116a
S8 I Group Acct <8idiuser@talc.xray.aps.anl.gov>
2020-07-14 13:37:48 -0500

    bug fix

--- run_gui.sh
+++ run_gui.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+LOG_FILE=.log
+
+date >> $LOG_FILE
+echo "start new gui instance" >> $LOG_FILE
+echo "arguments: " "$@" >> $LOG_FILE
 
 if [ "$1" != "" ]; then
     if [ "$1" == "--update" ]; then
@@ -8,14 +13,18 @@ if [ "$1" != "" ]; then
     fi
     target_folder=$(realpath $1)
 else
-    target_folder=`pwd`
+    target_folder=$(pwd)
 fi
 
-if [ -d "/path/to/dir" ]; then
+echo "target folder $target_folder" >> $LOG_FILE
+
+if [ -d $target_folder ]; then
     WD=/home/beams/8IDIUSER/Documents/Miaoqi/xpcs_gui
     cd $WD
     /APSshare/anaconda3/x86_64/bin/python gui.py $target_folder &
 else
     echo "no such directory: " $1 or $target_folder
 fi
+
+echo "      " >> $LOG_FILE
 
