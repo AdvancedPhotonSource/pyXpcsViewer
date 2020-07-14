c277cae5a0d82b33fb0624ff803321c04a097cc8
S8 I Group Acct <8idiuser@talc.xray.aps.anl.gov>
2020-07-14 10:24:18 -0500

    change to absolute path

--- run_gui.sh
+++ run_gui.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 if [ "$1" != "" ]; then
-    target_folder=$1
+    if [ "$1" == "--update" ]; then
+        echo "update xpcs_gui"
+        git pull
+        exit
+    fi
+    target_folder=$(realpath $1)
 else
     target_folder=`pwd`
 fi
 
 WD=/home/beams/8IDIUSER/Documents/Miaoqi/xpcs_gui
 cd $WD
-
 /APSshare/anaconda3/x86_64/bin/python gui.py $target_folder &
+
