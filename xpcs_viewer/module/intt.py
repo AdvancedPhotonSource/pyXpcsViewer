e93f08c0f52fe85d0cdb11bbefc83b8a3b9ae004
Miaoqi Chu mac <mqichu@anl.gov>
2020-10-24 22:36:45 -0500

    separate intt and g2

---
+++ xpcs_viewer/module/intt.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+def smooth_data(fc, window, sampling):
+    x, y = fc.Int_t[0], fc.Int_t[1]
+    if window > 1:
+        y = np.cumsum(y, dtype=float, axis=0)
+        y = (y[window:] - y[:-window]) / window
+        x = x[window:]
+    if sampling >= 2:
+        y = y[::sampling]
+        x = x[::sampling]
+
+    return x, y
+
+
+def plot(xf_list, pg_hdl, legend, **kwargs):
+    data = []
+    for fc in xf_list:
+        x, y = smooth_data(fc, **kwargs)
+        data.append([x, y])
+
+    pg_hdl.clear()
+    pg_hdl.show_lines(data,
+                      xlabel="Time (s)",
+                      ylabel="Intensity (ph/pixel)",
+                      loc='lower right',
+                      alpha=0.5,
+                      legend=legend)
+    pg_hdl.draw()
+
