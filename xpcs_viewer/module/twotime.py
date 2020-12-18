a59a50a6c42b22e86f4c34badade4fb6caa04c94
Miaoqi Chu mac <mqichu@anl.gov>
2020-12-18 15:27:49 -0600

    implement two plots for 2time

--- xpcs_viewer/module/twotime.py
+++ xpcs_viewer/module/twotime.py
@@ -1,9 +1,47 @@
 import numpy as np
-import os
 import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
 
 
-def plot_twotime_map(xfile, hdl, meta=None, group='xpcs', cmap='jet', scale='log',
+def get_twotime_qindex(meta, ix, iy, hdl):
+    shape = meta['twotime_dqmap'].shape
+    if len(hdl.axes[0].patches) >= 2:
+        hdl.axes[0].patches.pop(0)
+    if len(hdl.axes[1].patches) >= 2:
+        hdl.axes[1].patches.pop(0)
+
+    h = np.argmin(np.abs(np.arange(shape[1]) - ix))
+    v = np.argmin(np.abs(np.arange(shape[0]) - iy))
+    color = np.random.rand(3, )
+    mark0 = Circle((ix, iy), radius=5, edgecolor='white', facecolor=color)
+    hdl.axes[0].add_patch(mark0)
+    if meta['twotime_text'] is None:
+        text_a = hdl.axes[0].text(ix, iy, 'A')
+        text_b = hdl.axes[0].text(ix, iy, 'A')
+        meta['twotime_text'] = [text_a, text_b]
+    else:
+        text_b = meta['twotime_text'][1]
+        pos = text_b.get_position()
+        text_a = meta['twotime_text'][0]
+        text_a.set_position(pos)
+
+        text_b.set_position((ix, iy))
+        text_b.set_text('B')
+
+    mark1 = Circle((ix, iy), radius=5, edgecolor='white', facecolor=color)
+    hdl.axes[1].add_patch(mark1)
+    hdl.draw()
+    meta['twotime_pos'] = (v, h)
+
+    return meta['twotime_dqmap'][v, h]
+
+
+def plot_twotime_map(xfile,
+                     hdl,
+                     meta=None,
+                     group='xpcs',
+                     cmap='jet',
+                     scale='log',
                      auto_crop=True):
 
     if xfile.full_path == meta['twotime_fname'] and \
@@ -61,9 +99,17 @@ def plot_twotime(xfile, hdl, meta, plot_index=1, cmap='jet'):
         ret = (np.mean(v), np.mean(h))
     else:
         ret = None
-    
+
     if xfile.type != 'Twotime':
         return ret
+
+    if 'twotime_plot_index' not in meta:
+        meta['twotime_plot_index'] = plot_index
+    else:
+        if meta['twotime_plot_index'] == plot_index:
+            return
+        else:
+            meta['twotime_plot_index'] = plot_index
 
     c2 = xfile.get_twotime_c2(plot_index=plot_index,
                               twotime_key=meta['twotime_key'])
@@ -72,16 +118,30 @@ def plot_twotime(xfile, hdl, meta, plot_index=1, cmap='jet'):
     t_min = np.min(t)
     t_max = np.max(t)
 
+    if 'twotime_ims' not in meta:
+        meta['twotime_ims'] = []
+
     hdl.clear()
-    ax = hdl.subplots(1, 2)
-    im = ax[0].imshow(c2,
-                      interpolation='none',
-                      origin='lower',
-                      extent=([t_min, t_max, t_min, t_max]),
-                      cmap=plt.get_cmap(cmap))
-    plt.colorbar(im, ax=ax[0])
-    ax[0].set_ylabel('t1 (s)')
-    ax[0].set_xlabel('t2 (s)')
+    if len(meta['twotime_ims']) == 0:
+        c2_list = [c2]
+        meta['twotime_ims'].append(np.copy(c2))
+        ax = hdl.subplots(1, 2)
+
+    elif len(meta['twotime_ims']) == 1:
+        c2_list = [np.copy(meta['twotime_ims'][0]), c2]
+        meta['twotime_ims'].pop(0)
+        meta['twotime_ims'].append(np.copy(c2))
+        ax = hdl.subplots(1, 3)
+
+    for n in range(len(c2_list)):
+        im = ax[n].imshow(c2_list[n],
+                          interpolation='none',
+                          origin='lower',
+                          extent=([t_min, t_max, t_min, t_max]),
+                          cmap=plt.get_cmap(cmap))
+        plt.colorbar(im, ax=ax[n])
+        ax[n].set_ylabel('t1 (s)')
+        ax[n].set_xlabel('t2 (s)')
 
     # the first element in the list seems to deviate from the rest a lot
     g2f = xfile.g2_full[:, plot_index - 1][1:]
@@ -89,13 +149,13 @@ def plot_twotime(xfile, hdl, meta, plot_index=1, cmap='jet'):
     g2p = g2p[:, 1:]
 
     t = meta['twotime_scale'] * np.arange(g2f.size)
-    ax[1].plot(t, g2f, lw=3, color='blue', alpha=0.5)
+    ax[-1].plot(t, g2f, lw=3, color='blue', alpha=0.5)
     for n in range(g2p.shape[0]):
         t = meta['twotime_scale'] * np.arange(g2p[n].size)
-        ax[1].plot(t, g2p[n], label='partial%d' % n, alpha=0.5)
-    ax[1].set_xscale('log')
-    ax[1].set_ylabel('g2')
-    ax[1].set_xlabel('t (s)')
+        ax[-1].plot(t, g2p[n], label='partial%d' % n, alpha=0.5)
+    ax[-1].set_xscale('log')
+    ax[-1].set_ylabel('g2')
+    ax[-1].set_xlabel('t (s)')
     hdl.fig.tight_layout()
 
     hdl.draw()
