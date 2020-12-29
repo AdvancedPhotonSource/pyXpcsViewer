616c5cf064fda8fcca5c5323217eef06899d9244
Miaoqi Chu linux <mqichu@anl.gov>
2020-12-29 14:12:42 -0600

    icon/ui fix

--- xpcs_viewer/__init__.py
+++ xpcs_viewer/__init__.py
@@ -2,8 +2,9 @@
 from .viewer import run 
 from .viewer_kernel import ViewerKernel
 from .xpcs_file import XpcsFile as xf
+from . import icons_rc
 
-__all__ = [run, xf]
+__all__ = [run, xf, icons_rc]
 __version__ = '0.1.0'
 __author__ = 'Miaoqi Chu'
 __credits__ = 'Argonne National Laboratory'
