eddf1e2566bb4057ddc66c2dc141f21edb4a4933
Miaoqi Chu linux <mqichu@anl.gov>
2020-12-31 11:35:48 -0600

    change icons location

--- update_ui.sh
+++ update_ui.sh
@@ -1,5 +1,8 @@
 WD=xpcs_viewer
 # ui file
-pyuic5 $WD/ui/xpcs.ui -o $WD/viewer_ui.py
+pyuic5 $WD/ui/xpcs.ui -o viewer_ui.py
 # resource file goes to the current level
-pyrcc5 $WD/ui/resources/icons.qrc -o icons_rc.py
+pyrcc5 $WD/ui/resources/icons.qrc -o $WD/icons_rc.py
+sed 's/import icons_rc.*/from . import icons_rc/' viewer_ui.py > $WD/viewer_ui.py
+rm viewer_ui.py 
+
