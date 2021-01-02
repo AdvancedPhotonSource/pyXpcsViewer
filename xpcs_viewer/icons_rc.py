54c4dc1dcc2a649fce139dc99305d3ee859e88cd
Miaoqi Chu mac <mqichu@anl.gov>
2021-01-01 22:40:58 -0600

    ui update

--- xpcs_viewer/icons_rc.py
+++ xpcs_viewer/icons_rc.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created by: The Resource Compiler for PyQt5 (Qt v5.9.7)
+# Created by: The Resource Compiler for PyQt5 (Qt v5.15.1)
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -202,11 +202,11 @@ qt_resource_struct_v2 = b"\
 \x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x02\
 \x00\x00\x00\x00\x00\x00\x00\x00\
 \x00\x00\x00\x10\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
-\x00\x00\x01\x76\xac\x32\xc1\x5c\
+\x00\x00\x01\x76\xaf\xf2\xd8\x53\
 "
 
-qt_version = QtCore.qVersion().split('.')
-if qt_version < ['5', '8', '0']:
+qt_version = [int(v) for v in QtCore.qVersion().split('.')]
+if qt_version < [5, 8, 0]:
     rcc_version = 1
     qt_resource_struct = qt_resource_struct_v1
 else:
