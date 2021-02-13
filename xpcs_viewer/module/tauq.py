2dcd088860f84fc1728789c46828780f79b7eaa2
Miaoqi Chu mac <mqichu@anl.gov>
2021-02-12 21:03:23 -0600

    add pre plot; change plot style

--- xpcs_viewer/module/tauq.py
+++ xpcs_viewer/module/tauq.py
@@ -2,49 +2,26 @@ import numpy as np
 from ..helper.fitting import fit_tau
 
 
-def plot(g2_fit_val, labels=None, max_q=0.016, hdl=None, offset=None):
-    if g2_fit_val is None:
-        msg = 'g2 fitting not ready'
-        return [msg]
-
-    num_points = len(g2_fit_val)
-    if num_points == 0:
-        msg = 'g2 fitting not ready'
-        return [msg]
-
-    # prepare fit values
-    fit_val = []
-    for _, val in g2_fit_val.items():
-        fit_val.append(val)
-    fit_val = np.hstack(fit_val).swapaxes(0, 1)
-    q = fit_val[::7]
-    sl = q[0] <= max_q
-
-    tau = fit_val[1::7]
-    cts = fit_val[3::7]
-
-    tau_err = fit_val[4::7]
-    cts_err = fit_val[6::7]
-
-    fit_msg = []
+def plot(xf_list, hdl, q_range, offset):
 
     hdl.clear()
     ax = hdl.subplots(1, 1)
 
-    for n in range(tau.shape[0]):
+    n = -1
+    for xf in xf_list:
+        n += 1
         s = 10 ** (offset * n)
-        line = ax.errorbar(q[n][sl],
-                           tau[n][sl] / s,
-                           yerr=tau_err[n][sl] / s,
-                           fmt='o-',
-                           markersize=3,
-                           label=labels[n])
-        slope, intercept, xf, yf = fit_tau(q[n][sl], tau[n][sl],
-                                           tau_err[n][sl])
-        # plot straight line
-        ax.plot(xf, yf / s)
-        fit_msg.append('fn: %s, slope = %.4f, intercept = %.4f' %
-                       (labels[n], slope, intercept))
+        x = xf.fit_summary['q_val']
+        y = xf.fit_summary['fit_val'][:, 0, 1]
+        e = xf.fit_summary['fit_val'][:, 1, 1]
+        line = ax.errorbar(x, y/s,  yerr=e/s, fmt='o-', markersize=3,
+                           label=xf.label)
+        fit_x = xf.fit_summary['tauq_fit_line']['fit_x']
+        fit_y = xf.fit_summary['tauq_fit_line']['fit_y']
+
+        ax.plot(fit_x, fit_y / s)
+        # fit_msg.append('fn: %s, slope = %.4f, intercept = %.4f' %
+        #                (labels[n], slope, intercept))
 
     ax.set_xlabel('$q (\\AA^{-1})$')
     ax.set_ylabel('$\\tau \\times 10^4$')
@@ -53,4 +30,47 @@ def plot(g2_fit_val, labels=None, max_q=0.016, hdl=None, offset=None):
     ax.set_yscale('log')
     hdl.draw()
 
-    return fit_msg
+    return
+
+
+def plot_pre(xf_list, hdl):
+
+    hdl.clear()
+    ax = hdl.subplots(2, 2, sharex=True).flatten()
+    titles = ['contrast', 'tau (s)', 'stretch', 'baseline']
+
+    for idx, xf in enumerate(xf_list):
+        for n in range(4):
+            x = xf.fit_summary['q_val']
+            y = xf.fit_summary['fit_val'][:, 0, n]
+            e = xf.fit_summary['fit_val'][:, 1, n]
+            ax[n].errorbar(x, y,  yerr=e, fmt='o-', markersize=3,
+                           label=xf.label)
+
+        if idx == 0:
+            bounds = xf.fit_summary['bounds']
+            xmin, xmax = np.min(x), np.max(x)
+            for n in range(4):
+                ymin = bounds[0][n]
+                ymax = bounds[1][n]
+                ax[n].set_title(titles[n])
+                ax[n].set_title(titles[n])
+
+                if n == 1:
+                    ax[n].set_yscale('log')
+                    ax[n].set_ylim(ymin * 0.8, ymax * 1.2)
+                else:
+                    ax[n].set_ylim(ymin * 0.8, ymax * 1.2)
+
+                if n > 1:
+                    ax[n].set_xlabel('$q (\\AA^{-1})$')
+                # add two lines showing the fitting ub and lb
+                ax[n].hlines(ymin, xmin, xmax, color='b', label='lower bound')
+                ax[n].hlines(ymax, xmin, xmax, color='g', label='upper bound')
+
+                # only show legend in the last plot
+                if n == 3:
+                    ax[n].legend()
+    hdl.draw()
+
+    return
