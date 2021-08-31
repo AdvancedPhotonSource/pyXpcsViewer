9549863bd352da0b23ff31cfb4e27d734c7aeeba
Miaoqi Chu mac <mqichu@anl.gov>
2021-08-31 01:22:50 -0500

    move show_lines to saxs1d

--- xpcs_viewer/module/saxs1d.py
+++ xpcs_viewer/module/saxs1d.py
@@ -1,5 +1,7 @@
+from build.lib.xpcs_viewer.plothandler import matplot_qt
 import numpy as np
 from .g2mod import create_slice
+from ..plothandler.matplot_qt import get_color_marker
 
 
 def offset_intensity(Iq, n, plot_offset=None, yscale=None):
@@ -70,7 +72,8 @@ def switch_line_builder(hdl, state):
     elif state == False and hdl.line_builder is not None:
         hdl.unlink_line_builder()
 
-def plot(xf_list, mp_hdl, plot_type=2, plot_norm=0, plot_offset=0,
+
+def plot2(xf_list, mp_hdl, plot_type=2, plot_norm=0, plot_offset=0,
          max_points=8, title=None, rows=None, qmax=10.0, qmin=0,
          loc='best', marker_size=3, sampling=1):
 
@@ -95,5 +98,56 @@ def plot(xf_list, mp_hdl, plot_type=2, plot_norm=0, plot_offset=0,
 
     mp_hdl.axes.set_title(title)
     mp_hdl.auto_scale(xscale=xscale, yscale=yscale)
+    mp_hdl.draw()
+    return
+
+
+def plot(xf_list, mp_hdl, plot_type=2, plot_norm=0, plot_offset=0,
+         max_points=8, title=None, rows=None, qmax=10.0, qmin=0,
+         loc='best', marker_size=3, sampling=1):
+
+    xscale = ['linear', 'log'][plot_type % 2]
+    yscale = ['linear', 'log'][plot_type // 2]
+
+    mp_hdl.clear()
+    ax = mp_hdl.subplots(1, 1)
+
+    if rows in [None, []]:
+        alpha = np.ones(len(xf_list)) * 0.75
+    else:
+        alpha = np.ones(len(xf_list)) * 0.15
+        for t in rows:
+            if t < len(xf_list):
+                alpha[t] = 1.0
+
+    plot_id = 0
+    for n, fi in enumerate(xf_list[slice(0, max_points)]):
+        Iq, q = fi.saxs_1d['Iq'], fi.saxs_1d['q']
+        # apply sampling
+        Iq, q = Iq[:, ::sampling], q[::sampling]
+
+        # apply qrange
+        sl = create_slice(q, (qmin, qmax))
+        Iq = Iq[:, sl]
+        q = q[sl]
+
+        for m in range(Iq.shape[0]):
+            cl, mk = get_color_marker(plot_id)
+            Iqm = offset_intensity(Iq[m], plot_id, plot_offset, yscale)
+            Iqm, _, xlabel, ylabel = norm_saxs_data(Iqm, q, plot_norm)
+            ax.plot(q, Iqm, mk + '-', label=fi.saxs_1d['labels'][m],
+                    ms=marker_size, alpha=alpha[n], color=cl, mfc='none')
+            plot_id += 1
+        
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_title(title)
+    mp_hdl.auto_scale(xscale=xscale, yscale=yscale)
+    if loc != 'outside':
+        ax.legend(loc=loc)
+    elif loc == 'outside':
+        ax.legend(bbox_to_anchor=(1.03, 1.0), loc='upper left')
+    mp_hdl.fig.tight_layout(rect=(0.05, 0.05, 0.95, 0.95))
+
     mp_hdl.draw()
     return
