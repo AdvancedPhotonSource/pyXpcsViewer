66520020ccb4f7da65010eb950189300edd8526f
Miaoqi Chu wsl <mqichu@anl.gov>
2022-05-10 12:10:41 -0500

    add a pie-roi method

--- xpcs_viewer/plothandler/pyqtgraph_handler.py
+++ xpcs_viewer/plothandler/pyqtgraph_handler.py
@@ -2,7 +2,9 @@ import pyqtgraph as pg
 from pyqtgraph import ImageView, GraphicsLayoutWidget
 from .mpl_cmaps_in_ImageItem import pg_get_cmap
 import matplotlib.pyplot as plt
-from PyQt5 import QtCore
+# from PyQt5 import QtCore, QtGui
+from pyqtgraph import QtGui, QtCore
+import numpy as np
 
 
 pg.setConfigOptions(imageAxisOrder='row-major')
@@ -11,6 +13,8 @@ pg.setConfigOptions(imageAxisOrder='row-major')
 class ImageViewDev(ImageView):
     def __init__(self, *args, **kwargs) -> None:
         super(ImageViewDev, self).__init__(*args, **kwargs)
+        self.roi_record = {}
+        self.roi_idx = 0
 
     def adjust_viewbox(self):
         vb = self.getView()
@@ -99,12 +103,103 @@ class ImageViewDev(ImageView):
 
     def clear(self):
         super(ImageViewDev, self).clear()
+        self.remove_rois()
         self.reset_limits()
         # incase the signal isn't connected to anything.
         try:
             self.scene.sigMouseMoved.disconnect()
         except:
             pass
+    
+    def add_roi(self, cen=None, num_edges=None, radius=60, color='r',
+                sl_type='Pie', width=3, sl_mode='exclusive',
+                second_point=None, label=None):
+        # label: label of roi; default is None, which is for roi-draw
+
+        if label is not None and label in self.hdl.roi:
+            self.hdl.remove_item(label)
+
+        if sl_mode == 'inclusive':
+            pen = pg.mkPen(color=color, width=width, style=QtCore.Qt.DotLine)
+        else:
+            pen = pg.mkPen(color=color, width=width)
+
+        kwargs = {
+            'pen': pen,
+            'removable': True,
+            'hoverPen': pen,
+            'handlePen': pen
+        }
+        if sl_type == 'Ellipse':
+            new_roi = pg.EllipseROI(cen, [60, 80], **kwargs)
+            # add scale handle
+            new_roi.addScaleHandle([0.5, 0], [0.5, 1], )
+            new_roi.addScaleHandle([0.5, 1], [0.5, 0])
+            new_roi.addScaleHandle([0, 0.5], [1, 0.5])
+            new_roi.addScaleHandle([1, 0.5], [0, 0.5])
+
+        elif sl_type == 'Circle':
+            if second_point is not None:
+                radius = np.sqrt((second_point[1] - cen[1]) ** 2 + 
+                                 (second_point[0] - cen[0]) ** 2)
+            new_roi = pg.CircleROI(pos=[cen[0] - radius, cen[1] - radius],
+                                   radius=radius,
+                                   **kwargs)
+
+        elif sl_type == 'Polygon':
+            if num_edges is None:
+                num_edges = np.random.random_integers(6, 10)
+
+            # add angle offset so that the new rois don't overlap with each
+            # other
+            offset = np.random.random_integers(0, 359)
+            theta = np.linspace(0, np.pi * 2, num_edges + 1) + offset
+            x = radius * np.cos(theta) + cen[0]
+            y = radius * np.sin(theta) + cen[1]
+            pts = np.vstack([x, y]).T
+            new_roi = pg.PolyLineROI(pts, closed=True, **kwargs)
+
+        elif sl_type == 'Rectangle':
+            new_roi = pg.RectROI(cen, [30, 150], **kwargs)
+            new_roi.addScaleHandle([0, 0], [1, 1])
+            # new_roi.addRotateHandle([0, 1], [0.5, 0.5])
+        
+        elif sl_type == 'Line':
+            if second_point is None:
+                return
+            width = kwargs.pop('width', 1)
+            new_roi = pg.LineROI(cen, second_point, width,
+                              **kwargs)
+        elif sl_type == 'Pie':
+            width = kwargs.pop('width', 1)
+            new_roi = PieROI(cen, radius, **kwargs)
+        else:
+            raise TypeError('type not implemented. %s' % sl_type)
+
+        new_roi.sl_mode = sl_mode
+
+        if label is None:
+            label = f"roi_{self.roi_idx:06d}"
+            self.roi_idx += 1
+        self.roi_record[label] = new_roi
+        self.addItem(new_roi)
+        new_roi.sigRemoveRequested.connect(lambda: self.remove_roi(label))
+
+        return label 
+    
+    def remove_rois(self, filter_str=None):
+        # if filter_str is None; then remove all rois
+        keys = list(self.roi_record.keys()).copy()
+        if filter_str is not None:
+            keys = list(filter(lambda x: x.startswith(filter_str), keys))
+        for key in keys:
+            self.remove_roi(key)
+    
+    def remove_roi(self, roi_key):
+        t = self.roi_record.pop(roi_key, None)
+        if t is not None:
+            self.removeItem(t)
+    
 
 
 class PlotWidgetDev(GraphicsLayoutWidget):
@@ -123,3 +218,85 @@ class PlotWidgetDev(GraphicsLayoutWidget):
         width = self.width()
         canvas_size = max(min_size, int(width / num_col * aspect * num_row))
         self.setMinimumSize(QtCore.QSize(0, canvas_size))
+
+
+
+class PieROI(pg.ROI):
+    r"""
+    Equilateral triangle ROI subclass with one scale handle and one rotation handle.
+    Arguments
+    pos            (length-2 sequence) The position of the ROI's origin.
+    size           (float) The length of an edge of the triangle.
+    \**args        All extra keyword arguments are passed to ROI()
+    ============== =============================================================
+    """
+
+    def __init__(self, pos, size, **args):
+        cen = (pos[0], pos[1] - size / 2.0)
+        pg.ROI.__init__(self, cen, [size, size], aspectLocked=False, 
+                        movable=False, **args)
+        # _updateView is a rendering method inherited; used here to force
+        # update the view
+        self.sigRegionChanged.connect(self._updateView)
+        self.poly = None
+        self.half_angle = None
+        self.create_poly()
+        self.addScaleRotateHandle([1.0, 0], [0, 0.5])
+        self.addScaleHandle([1.0, 1.0], [0, 0.5])
+
+    def create_poly(self, width=1.0, height=1.0):
+        radius = np.hypot(width, height / 2.0)
+        max_angle = np.arcsin(height / 2.0 / radius)
+        angle = np.linspace(-max_angle, max_angle, 16)
+        self.half_angle = np.rad2deg(max_angle)
+
+        x = radius * np.cos(angle)
+        y = radius * np.sin(angle)
+        # make the x[0] and x[-1] two vertices at x = width after scaling
+        x = x / np.abs(x[0])
+        # make the y range to be height after scaling
+        y = y / (np.max(y) - np.min(y)) + 0.5
+        poly = QtGui.QPolygonF()
+        poly.append(QtCore.QPointF(0.0, 0.5))
+        for pt in zip(x, y):
+            poly.append(QtCore.QPointF(*pt))
+        self.poly = None
+        self.poly = poly
+    
+    def get_parameter(self):
+        state = self.getState()
+        angle_range = np.array([-1, 1]) * self.half_angle + state['angle'] 
+        # shift angle_range's origin to 6 clock
+        angle_range = angle_range - 90
+        angle_range = angle_range - np.floor(angle_range / 360.0) * 360.0
+        size = state['size']
+        dist = np.hypot(size[0], size[1] / 2.0)
+        ret = {
+            'dist': dist,
+            'angle_range': angle_range,
+            'pos': tuple(self.pos())
+        }
+        return ret
+
+    def paint(self, p, *args):
+        r = self.boundingRect()
+        p.setRenderHint(QtGui.QPainter.RenderHint.Antialiasing)
+        p.scale(r.width(), r.height())
+        p.setPen(self.currentPen)
+        p.drawPolygon(self.poly)
+
+    def shape(self):
+        self.path = QtGui.QPainterPath()
+        r = self.boundingRect()
+        # scale the path to match whats on the screen
+        t = QtGui.QTransform()
+        t.scale(r.width(), r.height())
+
+        width = r.width()
+        height = r.height()
+        self.create_poly(width, height)
+        self.path.addPolygon(self.poly)
+        return t.map(self.path)
+
+    def getArrayRegion(self, *args, **kwds):
+        return self._getArrayRegionForArbitraryShape(*args, **kwds)
