e26ea5d23de7f98c643e406f4087a0f6a25462c1
Miaoqi Chu <mqichu@anl.gov>
2025-03-31 14:46:28 -0500

    syntax update

--- src/pyxpcsviewer/module/twotime_utils.py
+++ src/pyxpcsviewer/module/twotime_utils.py
@@ -4,12 +4,12 @@ from functools import lru_cache
 from multiprocessing import Pool
 from ..fileIO.aps_8idi import key as key_map
 
-key_map = key_map['nexus']
+key_map = key_map["nexus"]
 
 
 def get_single_c2(args):
     full_path, index_str, max_size = args
-    c2_prefix = key_map['c2_prefix']
+    c2_prefix = key_map["c2_prefix"]
     with h5py.File(full_path, "r") as f:
         c2_half = f[f"{c2_prefix}/{index_str}"][()]
         c2 = c2_half + np.transpose(c2_half)
@@ -18,25 +18,24 @@ def get_single_c2(args):
         sampling_rate = 1
         if max_size > 0 and max_size < c2.shape[0]:
             sampling_rate = (c2.shape[0] + max_size - 1) // max_size
-            c2 = c2[::sampling_rate, ::sampling_rate] 
+            c2 = c2[::sampling_rate, ::sampling_rate]
     return c2, sampling_rate
 
 
 @lru_cache(maxsize=16)
-def get_c2_from_hdf_fast(full_path, dq_selection=None, max_c2_num=32,
-                              max_size=512, num_workers=12):
+def get_c2_from_hdf_fast(
+    full_path, dq_selection=None, max_c2_num=32, max_size=512, num_workers=12
+):
     # t0 = time.perf_counter()
     idx_toload = []
-    c2_prefix = key_map['c2_prefix']
-    g2_full_key = key_map['c2_g2']                 # Dataset {5000, 25}
-    g2_partial_key = key_map['c2_g2_segments']     # Dataset {1000, 5, 25} 
-    acquire_period_key = key_map['t0']
+    c2_prefix = key_map["c2_prefix"]
+    g2_full_key = key_map["c2_g2"]  # Dataset {5000, 25}
+    g2_partial_key = key_map["c2_g2_segments"]  # Dataset {1000, 5, 25}
 
     with h5py.File(full_path, "r") as f:
         if c2_prefix not in f:
             return None
         idxlist = list(f[c2_prefix])
-        acquire_period = f[acquire_period_key][()]
         g2_full = f[g2_full_key][()]
         g2_partial = f[g2_partial_key][()]
         for idx in idxlist:
@@ -58,14 +57,16 @@ def get_c2_from_hdf_fast(full_path, dq_selection=None, max_c2_num=32,
 
     c2_all = np.array([res[0] for res in result])
     sampling_rate_all = set([res[1] for res in result])
-    assert len(sampling_rate_all) == 1, f"Sampling rate not consistent {sampling_rate_all}"
+    assert (
+        len(sampling_rate_all) == 1
+    ), f"Sampling rate not consistent {sampling_rate_all}"
     sampling_rate = list(sampling_rate_all)[0]
     c2_result = {
         "c2_all": c2_all,
         "g2_full": g2_full,
         "g2_partial": g2_partial,
-        "delta_t": acquire_period * sampling_rate,
-        "acquire_period": acquire_period,
+        "delta_t": 1.0 * sampling_rate,  # put absolute time in xpcs_file
+        "acquire_period": 1.0,
         "dq_selection": dq_selection,
     }
     return c2_result
@@ -73,7 +74,7 @@ def get_c2_from_hdf_fast(full_path, dq_selection=None, max_c2_num=32,
 
 def get_c2_stream(full_path, max_size=-1):
     """Returns (idxlist, generator) where the generator yields C2 streams."""
-    c2_prefix = key_map['c2_prefix']
+    c2_prefix = key_map["c2_prefix"]
 
     with h5py.File(full_path, "r") as f:
         if c2_prefix in f:
