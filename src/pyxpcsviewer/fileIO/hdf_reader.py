d0c25074c6523c9fac1915933d27084645379d38
Miaoqi Chu <mqichu@anl.gov>
2025-05-07 00:52:16 -0500

    fix (1, 1) shape

--- src/pyxpcsviewer/fileIO/hdf_reader.py
+++ src/pyxpcsviewer/fileIO/hdf_reader.py
@@ -8,7 +8,7 @@ import re
 logger = logging.getLogger(__name__)
 
 
-def put(save_path, result, ftype='nexus', mode='raw'):
+def put(save_path, result, ftype="nexus", mode="raw"):
     """
     save the result to hdf5 file
     Parameters
@@ -22,9 +22,9 @@ def put(save_path, result, ftype='nexus', mode='raw'):
     mode: str
         'raw' or 'alias'
     """
-    with h5py.File(save_path, 'a') as f:
+    with h5py.File(save_path, "a") as f:
         for key, val in result.items():
-            if mode == 'alias':
+            if mode == "alias":
                 key = hdf_key[ftype][key]
             if key in f:
                 del f[key]
@@ -34,9 +34,9 @@ def put(save_path, result, ftype='nexus', mode='raw'):
         return
 
 
-def get_abs_cs_scale(fname, ftype='nexus'):
-    key = hdf_key[ftype]['abs_cross_section_scale']
-    with h5py.File(fname, 'r') as f:
+def get_abs_cs_scale(fname, ftype="nexus"):
+    key = hdf_key[ftype]["abs_cross_section_scale"]
+    with h5py.File(fname, "r") as f:
         if key not in f:
             return None
         else:
@@ -59,7 +59,7 @@ def read_metadata_to_dict(file_path):
     """
 
     def recursive_read(h5_group, target_dict):
-        """ Recursively reads groups and datasets into the dictionary. """
+        """Recursively reads groups and datasets into the dictionary."""
         for key in h5_group:
             obj = h5_group[key]
             if isinstance(obj, h5py.Dataset):
@@ -72,9 +72,14 @@ def read_metadata_to_dict(file_path):
                 recursive_read(obj, target_dict[key])
 
     data_dict = {}
-    groups = ['/entry/instrument', '/xpcs/multitau/config',
-              '/xpcs/twotime/config', '/entry/sample', '/entry/user']
-    with h5py.File(file_path, 'r') as hdf_file:
+    groups = [
+        "/entry/instrument",
+        "/xpcs/multitau/config",
+        "/xpcs/twotime/config",
+        "/entry/sample",
+        "/entry/user",
+    ]
+    with h5py.File(file_path, "r") as hdf_file:
         for group in groups:
             if group in hdf_file:
                 data_dict[group] = {}
@@ -82,7 +87,7 @@ def read_metadata_to_dict(file_path):
     return data_dict
 
 
-def get(fname, fields, mode='raw', ret_type='dict', ftype='nexus'):
+def get(fname, fields, mode="raw", ret_type="dict", ftype="nexus"):
     """
     get the values for the various keys listed in fields for a single
     file;
@@ -93,32 +98,34 @@ def get(fname, fields, mode='raw', ret_type='dict', ftype='nexus'):
     :param ret_type: return dictonary if 'dict', list if it is 'list'
     :return: dictionary or dictionary;
     """
-    assert mode in ['raw', 'alias'], 'mode not supported'
-    assert ret_type in ['dict', 'list'], 'ret_type not supported'
+    assert mode in ["raw", "alias"], "mode not supported"
+    assert ret_type in ["dict", "list"], "ret_type not supported"
 
     ret = {}
-    with h5py.File(fname, 'r') as HDF_Result:
+    with h5py.File(fname, "r") as HDF_Result:
         for key in fields:
-            path = hdf_key[ftype][key] if mode == 'alias' else key
+            path = hdf_key[ftype][key] if mode == "alias" else key
             if path not in HDF_Result:
-                logger.error('path to field not found: %s', path)
-                raise ValueError('key not found: %s:%s', key, path)
+                logger.error("path to field not found: %s", path)
+                raise ValueError("key not found: %s:%s", key, path)
             else:
                 val = HDF_Result.get(path)[()]
-            if key in ['saxs_2d']:  # saxs_2d is in [1xNxM] format
+            if key in ["saxs_2d"] and val.ndim == 3:  # saxs_2d is in [1xNxM] format
                 val = val[0]
             # converts bytes to unicode;
             if type(val) in [np.bytes_, bytes]:
                 val = val.decode()
+            if isinstance(val, np.ndarray) and val.shape == (1, 1):
+                val = val.item()
             ret[key] = val
 
-    if ret_type == 'dict':
+    if ret_type == "dict":
         return ret
-    elif ret_type == 'list':
+    elif ret_type == "list":
         return [ret[key] for key in fields]
 
 
-def get_analysis_type(fname, ftype='nexus'):
+def get_analysis_type(fname, ftype="nexus"):
     """
     determine the analysis type of the file
     Parameters
@@ -132,21 +139,19 @@ def get_analysis_type(fname, ftype='nexus'):
     tuple
         analysis type, 'Twotime' or 'Multitau', or both
     """
-    c2_prefix = hdf_key[ftype]['c2_prefix']
-    g2_prefix = hdf_key[ftype]['g2']
+    c2_prefix = hdf_key[ftype]["c2_prefix"]
+    g2_prefix = hdf_key[ftype]["g2"]
     analysis_type = []
-    with h5py.File(fname, 'r') as HDF_Result:
+    with h5py.File(fname, "r") as HDF_Result:
         if c2_prefix in HDF_Result:
-            analysis_type.append('Twotime')
+            analysis_type.append("Twotime")
         if g2_prefix in HDF_Result:
-            analysis_type.append('Multitau')
+            analysis_type.append("Multitau")
 
     if len(analysis_type) == 0:
-        raise ValueError(f'No analysis type found in {fname}')
+        raise ValueError(f"No analysis type found in {fname}")
     return tuple(analysis_type)
 
 
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     pass
